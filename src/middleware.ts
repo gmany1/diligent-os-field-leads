b4906da5898720dfe3e4e3ebf13eb2ba779fc99d
Jorge <Jorge101dev@gmail.com>
2025-12-03 16:18:51 -0600

    Security: Allow public access to /api/admin/reset for initial setup

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,5 +5,5 @@ export default NextAuth(authConfig).auth;
 
 export const config = {
     // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-    matcher: ['/((?!api/auth|_next/static|_next/image|.*\\.png$|manifest.webmanifest$).*)'],
+    matcher: ['/((?!api/auth|api/admin/reset|_next/static|_next/image|.*\\.png$|manifest.webmanifest$).*)'],
 };
